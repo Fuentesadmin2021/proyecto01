1824818c76535312ba1a71ac5f5fbecaf6900c76
Fuentesadmin2021 <edsanchezf@unal.edu.co>
2021-05-05 20:50:29 -0500

    Codigo para saber si un numero es primo

---
+++ programa01.py
@@ -0,0 +1,19 @@
+## programa para saber si un número es primo
+def es_primo(x):
+    cont=0
+    for i in range(2,x+1):
+        if x%i==0:
+            cont+=1
+            
+    if cont==1:
+        return True
+    return False
+
+num=int(input())
+
+
+if es_primo(num):
+    print('Numero primo')
+else:
+    print('Este numero no es primo')
+    
